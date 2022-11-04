26d1c928720df947bd34d3ee9a542a4b0fec887f
Viacheslav-Yurushev <108462275+ViacheslavYUR@users.noreply.github.com>
2022-11-05 00:51:31 +0200

    done

--- js/task-10.js
+++ js/task-10.js
@@ -28,14 +28,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const controlsRef = {
-  input : document.querySelector('input'),
-  createbtn : document.querySelector('[data-create]'),
-  destroybtn : document.querySelector('[data-destroy]'),
+
+const controls = document.querySelector('#controls');
+const input = controls.children[0];
+const create = controls.children[1];
+const destroy = controls.children[2];
+const boxes = document.querySelector('#boxes');
+
+let boxesAmount = 0;
+let markup = [];
+let size = 20;
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    size += 10;
+    markup.push(`<div id="boxes__item" style="background-color:${getRandomHexColor()}; height: ${size}px; width: ${size}px;"></div>`)
+  }
 }
 
-function createBoxes (amount) {
-  
-}
+input.addEventListener('click', () => boxesAmount = input.value);
 
-console.log(controlsRef)
+create.addEventListener('click', () => {
+  createBoxes(boxesAmount);
+  boxes.insertAdjacentHTML('afterbegin', markup.join(''));
+});
+
+destroy.addEventListener('click', () => {
+  boxes.innerHTML = '';
+  input.value = 0;
+  boxesAmount = 0;
+  markup = [];
+  size = 20;
+});
